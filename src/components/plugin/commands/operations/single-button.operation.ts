cb3ff043259cd7423a210cfe14ed3272d3ef5963
13701138894 <49673430+13701138894@users.noreply.github.com>
2024-10-22 16:35:02 +0800

    Update single-button.operation.ts

--- src/components/plugin/commands/operations/single-button.operation.ts
+++ src/components/plugin/commands/operations/single-button.operation.ts
@@ -1,16 +1,22 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const { CommandType, IUniverInstanceService, UniverInstanceType } = (window as any).UniverCore
+const { SheetsSelectionsService } = (window as any).UniverSheets
+
+const getSelectRange = (accessor) => {
+  const sheetSelectionManagerService = accessor.get(SheetsSelectionsService)
+  const currentSelection = sheetSelectionManagerService.getCurrentSelections()
+  const { startRow, endRow } = currentSelection[0].range
+  return { startRow, endRow }
+}
 
 export const SingleButtonOperation = {
   id: 'fx',
   type: CommandType.OPERATION,
   handler: async (accessor) => {
-    const univer = accessor.get(IUniverInstanceService)
-    const sheet = univer.getCurrentUnitForType(UniverInstanceType.UNIVER_SHEET).getActiveSheet()
-    const selection = sheet.getSelection()
-    const range = selection.getActiveRange()
-    console.log('range', range)
-    alert('分析')
+    const { startRow, endRow } = getSelectRange(accessor)
+    if (startRow === 0) return
+    if (startRow !== endRow) return showMessage('请选择一个主体分析')
+    // .... bus.emit('xxx')
     return true
   }
 }
@@ -18,11 +24,7 @@ export const pxButtonOperation = {
   id: 'px',
   type: CommandType.OPERATION,
   handler: async (accessor) => {
-    const univer = accessor.get(IUniverInstanceService)
-    const sheet = univer.getCurrentUnitForType(UniverInstanceType.UNIVER_SHEET).getActiveSheet()
-    const selection = sheet.getSelection()
-    const range = selection.getActiveRange()
-    console.log('range', range)
+
     alert('降序')
     return true
   }
@@ -31,11 +33,10 @@ export const reportButtonOperation = {
   id: 'report',
   type: CommandType.OPERATION,
   handler: async (accessor) => {
-    const univer = accessor.get(IUniverInstanceService)
-    const sheet = univer.getCurrentUnitForType(UniverInstanceType.UNIVER_SHEET).getActiveSheet()
-    const selection = sheet.getSelection()
-    const range = selection.getActiveRange()
-    console.log('range', range)
+    const { startRow, endRow } = getSelectRange(accessor)
+    if (startRow === 0) return
+    if (startRow !== endRow) return showMessage('请选择一个主体生成报告')
+    
     return true
   }
 }
